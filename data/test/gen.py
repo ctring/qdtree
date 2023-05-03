d15d280a7943d6209f8045cee7904f3da7513aa2
Cuong Nguyen <ctring23@gmail.com>
2023-05-03 01:39:39 -0400

    New test data

--- data/test/gen.py
+++ data/test/gen.py
@@ -3,15 +3,15 @@ import numpy as np
 import pandas as pd
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--rows", type=int, default=1000, help="Number of rows to generate")
+parser.add_argument("--rows", type=int, default=1000,
+                    help="Number of rows to generate")
 parser.add_argument("--out", type=str, default="test.csv", help="Output file")
 args = parser.parse_args()
 
 df = pd.DataFrame(
     {
-        "int_col": np.random.randint(0, 100, args.rows),
-        "float_col": np.random.rand(args.rows),
-        "date_col": pd.date_range("2020-01-01", periods=args.rows),
+        "cpu": np.random.randint(0, 100, args.rows),
+        "disk": np.random.rand(args.rows),
     }
 )
 df.to_csv(args.out, index=False)
